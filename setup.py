85d69731a6ee9544524b6e3d7f8a61b13bdad1c4
DealiAxy <dealiaxy@gmail.com>
2019-02-25 11:51:25 +0800

    修改：     setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
+# coding=utf-8
 from setuptools import setup
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-version = '0.1.19'
+version = '0.1.20'
 description = 'CloverSearch搜索引擎的Django模块，使用起来超级方便！'
 update = '优化建立索引、搜索性能，测试性能提高10倍以上，但是受限于Python语言的性能，数据量大的时候性能还是不够理想，有待进一步优化。'
 description = '{}。版本[{}]更新内容：{}'.format(description, version, update)
@@ -18,7 +19,7 @@ setup(
         'django',
         'ujson'
     ],
-    url='https://github.com/Deali-Axy',
+    url='https://github.com/Deali-Axy/CloverSearch',
     # license='GPLv3',
     author='DealiAxy',
     author_email='dealiaxy@gmail.com',
